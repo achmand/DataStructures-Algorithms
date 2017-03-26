a0ba350f569adbaf3ef60b8523704a23769e8aca
achmand- Dylan Vassallo <dylan.vassallo@hotmail.com>
2017-03-26 16:48:13 +0200

    Updated list adt

--- DataStructures/Interfaces/IListAdt.cs
+++ DataStructures/Interfaces/IListAdt.cs
@@ -4,9 +4,28 @@ namespace DataStructures.Interfaces
 {
     public interface IListAdt<T>
     {
-        bool IsEmpty();
+        // generic methods 
         int Size();
-        T ElementAtRank();
+        bool IsEmpty();
+
+        // query methods
+        void IsFirst(Node<T> node);
+        void IsLast(Node<T> node);
+
+        // accessor methods
+        T ElementAtRank(int rank);
+        Node<T> NodeAtRank(int rank);
+        Node<T> First();
+        Node<T> Last();
+        Node<T> PrevNode(Node<T> currNode);
+        Node<T> NextNode(Node<T> currNode);
+
+        // update methods 
+        T RemoveAtRank(int rank);
+        T RemoveAfter(Node<T> currNode, T element);
+        T RemoveBefore(Node<T> currNode, T element);
+        T ReplaceAtRank(int rank, T element);
+
         void InsertFirst(T element);
         void InsertAfter(Node<T> currNode, T element);
     }
