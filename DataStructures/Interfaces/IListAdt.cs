5323f49d5f3906cdbca9e46c3f54c24346dbc51f
achmand- Dylan Vassallo <dylan.vassallo@hotmail.com>
2017-03-26 15:49:42 +0200

    Implementing IListAdt

---
+++ DataStructures/Interfaces/IListAdt.cs
@@ -0,0 +1,13 @@
+﻿using DataStructures.LinkedLists;
+
+namespace DataStructures.Interfaces
+{
+    public interface IListAdt<T>
+    {
+        bool IsEmpty();
+        int Size();
+        T ElementAtRank();
+        void InsertFirst(T element);
+        void InsertAfter(Node<T> currNode, T element);
+    }
+}
---
+++ DataStructures/LinkedLists/Node.cs
@@ -0,0 +1,35 @@
+﻿namespace DataStructures.LinkedLists
+{
+    public class Node<T> 
+    {
+        public Node<T> Next { get; set; }
+        public T Element { get; set; }
+
+        public Node()
+        {
+
+        }
+
+        public Node(Node<T> next, T element)
+        {
+            Next = next;
+            Element = element;
+        }
+    }
+
+    public sealed class DoublyNode<T> : Node<T>
+    {
+        public Node<T> Prev { get; set; }
+
+        public DoublyNode()
+        {
+        }
+
+        public DoublyNode(Node<T> next, Node<T> prev, T element)
+        {
+            Next = next;
+            Prev = prev;
+            Element = element;
+        }
+    }
+}
